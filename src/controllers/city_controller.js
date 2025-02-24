9149d855f03e5bdbe96f7b7c21df90f8d278d20e
spidey-777 <spidey4952@gmail.com>
2025-02-24 17:24:58 +0530

    added city controller

---
+++ src/controllers/city_controller.js
@@ -0,0 +1,94 @@
+const {CityService, CityService} = require('../services/index');
+
+const cityService  = new CityService();
+
+
+const create = async (req,res) =>{
+    try {
+        const city = await cityService.createCity(req.body);
+        return res.status(201).json({
+            data : city,
+            success : true,
+            message : 'Successfully created city ',
+            err : {}
+        });
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data :{},
+            success : false,
+            err : error
+            
+        })
+    }
+}
+const destroy = async(req,res) =>{
+    try {
+        const response = await cityService.deleteCity(req.params.id);
+        return res.status(200).json({
+            data : response,
+            success : true,
+            message : 'Successfully deleted  city ',
+            err : {}
+        });
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data :{},
+            success : false,
+            err : error
+            
+        })
+    }
+}
+const update = async(req,res) =>{
+    try {
+        const response = await cityService.updateCity(req.params.id,req.body);
+        return res.status(200).json({
+            data : response,
+            success : true,
+            message : 'Successfully updated the city ',
+            err : {}
+        });
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data :{},
+            success : false,
+            err : error
+            
+        })
+    }
+}
+const get = async(req,res) =>{
+    try {
+        const response = await cityService.getCity(req.params.id);
+        return res.status(200).json({
+            data : response,
+            success : true,
+            message : 'Successfully fatched  city ',
+            err : {}
+        });
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data :{},
+            success : false,
+            err : error
+            
+        })
+    }
+}
+
+
+
+module.exports = {
+    create,
+    destroy,
+    update,
+    get
+}
